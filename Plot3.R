bd01d3944514d52e3db0a4e0efda0e949452dda6
Rossastoria <Rossastoria@users.noreply.github.com>
2016-10-07 21:45:03 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -2,4 +2,4 @@
 
 > p <- ggplot(aes(x = year, y = Emissions, fill = type), data = NEIBalt)
 > p + geom_bar(stat = "identity") + facet_grid(.~type) + labs(x = "year", y = "PM2.5 Emissions", title = "PM2.5 Emissions, Baltimore, by Source")
-> dev.copy(png, title = "Emissions_Plot3.png")
+> dev.copy(png, file = "Emissions_Plot3.png")
