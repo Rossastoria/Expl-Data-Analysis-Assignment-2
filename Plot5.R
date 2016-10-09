c4b90b0514261856436160f7dea9a16e9cf4e802
Rossastoria <Rossastoria@users.noreply.github.com>
2016-10-08 20:04:57 -0500

    Update Plot5.R

--- Plot5.R
+++ Plot5.R
@@ -1,3 +1,9 @@
 ## -- These commands create a plot of total PM2.5 emissions from vehicles in Baltimore
 
-  ## -- Subset out 
+  ## -- Create subseted data frame of the values (i.e., vehicles)
+> vehicle <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case = TRUE)      ## id vehicle emissions from SCC 
+> SCCVehicle <- SCC[vehicle,]$SCC                                         ## subset out id numbers from SCC
+> NEIVehicle <- NEI[NEI$SCC %in% SCCVehicle,]                             ## subset vehicles from NEI data
+
+  ## -- Subset out emissions from LA County and Baltimore
+> NEIVehBaltLA <- subset(NEIVehicle, fips == "24510")
